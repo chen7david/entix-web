78cc8eb9723c559a0b6592aaefb28610f0aca61c
David Chen <chen7david@me.com>
2025-01-01 14:24:59 -0500

    chore: changed to simpler import

--- src/config/amplify.ts
+++ src/config/amplify.ts
@@ -1,16 +1,14 @@
 import { Amplify } from "aws-amplify";
 
-export const configureAmplify = () => {
-  Amplify.configure({
-    Auth: {
-      Cognito: {
-        userPoolId: import.meta.env.VITE_COGNITO_USER_POOL_ID,
-        userPoolClientId: import.meta.env.VITE_COGNITO_CLIENT_ID,
-        loginWith: {
-          username: true,
-          email: true,
-        },
+export default Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: import.meta.env.VITE_COGNITO_USER_POOL_ID,
+      userPoolClientId: import.meta.env.VITE_COGNITO_CLIENT_ID,
+      loginWith: {
+        username: true,
+        email: true,
       },
     },
-  });
-};
+  },
+});
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
+import "./config/amplify";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { configureAmplify } from "./config/amplify";
-
-configureAmplify();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
