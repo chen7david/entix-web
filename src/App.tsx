c4001430f3320597eb656a3b8664ac8b2b0c75fb
David Chen <chen7daivd@me.com>
2025-06-01 17:10:52 -0400

    chore: setup antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { Button, message } from "antd";
 function App() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello World</h1>
+      <Button type="primary" onClick={() => message.success("Hello World")}>
+        Click me
+      </Button>
     </div>
   );
 }
